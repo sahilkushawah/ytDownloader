99cca5859306ec88cf32a17c12e3cb429ab141eb
sahil.cmarix <sahil.kushawah@cmarix.com>
2024-01-16 15:37:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const ytdl = require("ytdl-core");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+	return res.render("index");
+});
+
+app.get("/get", (req, res) => {
+    // Handle logic for /get route here
+    // For example, you can send a response or perform some action
+    res.send("This is the /get route");
+});
+
+app.get("/download", async (req, res) => {
+	const v_id = req.query.url.split('v=')[1];
+    const info = await ytdl.getInfo(req.query.url);
+
+    const videoFormats = info.formats.filter(format => !format.hasAudio);
+
+	return res.render("download", {
+		url: "https://www.youtube.com/embed/" + v_id,
+        info: videoFormats
+	});
+});
+app.listen(7000, () => {
+	console.log("Server is running on http://localhost:7000");
+});
